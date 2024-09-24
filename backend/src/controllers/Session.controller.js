af7a7e775c22c5cf6b8a4d5a1276cb9197cbc3c1
Adem Pelit <55463533+adem-pelit@users.noreply.github.com>
2024-09-25 02:59:19 +0300

    Session Controller has seperated from the Session router

---
+++ backend/src/controllers/Session.controller.js
@@ -0,0 +1,76 @@
+import { Router } from "express";
+import { questions } from "../utils/questions.js";
+import Session from "../models/Session.model.js";
+
+const GetCurrentSession = async (req, res) => {
+    req.session.ok = true;
+    console.log("req.session.id", req.session.id)
+    var currentSession = await Session.findOne({ id: req.session.id })
+    console.log("currentSession", currentSession)
+    if (currentSession == null) {
+        try {
+            var newSession = new Session({
+                id: req.session.id,
+                messages: [{ from: "bot", message: questions[0] }]
+            })
+            newSession.save()
+            res.json({ data: newSession })
+        } catch (err) {
+            res.json({ err })
+        }
+
+    }
+    else res.json({ data: currentSession })
+}
+
+const GetAllSessions = async (req, res) => {
+    var sessions = await Session.find()
+    res.json({ data: sessions })
+}
+
+const SendMessage = async (req, res) => {
+    const { message } = req.body
+    if (req.session.ok === undefined) {
+        res.status(500).json({ error: "req.session is undefined!", session: req.session })
+        return;
+    }
+
+    var currentSession = await Session.findOne({ id: req.session.id })
+    var botMessagesCount = currentSession.messages.filter(msg => msg.from === "bot").length;
+    if (questions.length < botMessagesCount) return res.json({ ok: true })
+    if (currentSession.messages.some(msg => msg.from === "end")) return res.json({ ok: true })
+    var messagesToAdd = [
+        { from: "me", message },
+        {
+            from: questions.length === botMessagesCount ? "end" : "bot",
+            message: questions.length === botMessagesCount ? "Thank you for answering questions session is closing in 5 seconds..." : questions[botMessagesCount]
+        }
+    ];
+    const updatedSession = await Session.findOneAndUpdate(
+        { id: req.session.id },
+        {
+            $push: {
+                messages: { $each: messagesToAdd }
+            }
+        },
+        { new: true }
+    );
+
+    if (!updatedSession) return res.status(404).json({ message: 'Session not found' });
+
+    res.json({ ok: true })
+}
+
+const LogoutSession = async (req, res) => {
+    const updatedSession = await Session.findOneAndUpdate(
+        { id: req.session.id },
+        { endTime: new Date() },
+        { new: true }
+    );
+    req.session.destroy((err) => {
+        if (err) return res.json({ ok: false, err });
+        else return res.json({ ok: true });
+    });
+}
+
+export { GetCurrentSession, GetAllSessions, SendMessage, LogoutSession };
--- backend/src/routes/Session.router.js
+++ backend/src/routes/Session.router.js
@@ -1,77 +1,11 @@
 import { Router } from "express";
-import { questions } from "../utils/questions.js";
-import Session from "../models/Session.model.js";
+import { GetAllSessions, GetCurrentSession, LogoutSession, SendMessage } from "../controllers/Session.controller.js";
+
 const sessionRouter = Router();
 
-sessionRouter.get("/current", async (req, res) => {
-    req.session.ok = true;
-    console.log("req.session.id", req.session.id)
-    var currentSession = await Session.findOne({ id: req.session.id })
-    console.log("currentSession", currentSession)
-    if (currentSession == null) {
-        try {
-            var newSession = new Session({
-                id: req.session.id,
-                messages: [{ from: "bot", message: questions[0] }]
-            })
-            newSession.save()
-            res.json({ data: newSession })
-        } catch (err) {
-            res.json({ err })
-        }
-
-    }
-    else res.json({ data: currentSession })
-});
-
-sessionRouter.get("/", async (req, res) => {
-    var sessions = await Session.find()
-    res.json({ data: sessions })
-});
-
-sessionRouter.post("/send-message", async (req, res) => {
-    const { message } = req.body
-    if (req.session.ok === undefined) {
-        res.status(500).json({ error: "req.session is undefined!", session: req.session })
-        return;
-    }
-
-    var currentSession = await Session.findOne({ id: req.session.id })
-    var botMessagesCount = currentSession.messages.filter(msg => msg.from === "bot").length;
-    if (questions.length < botMessagesCount) return res.json({ ok: true })
-    if (currentSession.messages.some(msg => msg.from === "end")) return res.json({ ok: true })
-    var messagesToAdd = [
-        { from: "me", message },
-        {
-            from: questions.length === botMessagesCount ? "end" : "bot",
-            message: questions.length === botMessagesCount ? "Thank you for answering questions session is closing in 5 seconds..." : questions[botMessagesCount]
-        }
-    ];
-    const updatedSession = await Session.findOneAndUpdate(
-        { id: req.session.id },
-        {
-            $push: {
-                messages: { $each: messagesToAdd }
-            }
-        },
-        { new: true }
-    );
-
-    if (!updatedSession) return res.status(404).json({ message: 'Session not found' });
-
-    res.json({ ok: true })
-});
-
-sessionRouter.get('/logout', async (req, res) => {
-    const updatedSession = await Session.findOneAndUpdate(
-        { id: req.session.id },
-        { endTime: new Date() },
-        { new: true }
-    );
-    req.session.destroy((err) => {
-        if (err) return res.json({ ok: false, err });
-        else return res.json({ ok: true });
-    });
-});
+sessionRouter.get("/current", GetCurrentSession);
+sessionRouter.get("/", GetAllSessions);
+sessionRouter.post("/send-message", SendMessage);
+sessionRouter.get('/logout', LogoutSession);
 
 export default sessionRouter;
