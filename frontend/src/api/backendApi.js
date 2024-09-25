098350cbb94e352172a143321c3abfbc7a765c68
Adem Pelit <55463533+adem-pelit@users.noreply.github.com>
2024-09-25 03:23:52 +0300

    error handling

--- frontend/src/api/backendApi.js
+++ frontend/src/api/backendApi.js
@@ -4,9 +4,11 @@ var backendApi = {
     session: {
         get() {
             return fetch(`${baseUrl}/session/current`, {
-                method:"GET",
+                method: "GET",
                 credentials: 'include'
-            }).then(res => res.json())
+            })
+                .then(res => res.json())
+                .catch(err => alert("" + err))
         },
         sendMesage(message) {
             return fetch(`${baseUrl}/session/send-message`, {
@@ -14,10 +16,14 @@ var backendApi = {
                 credentials: 'include',
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message })
-            }).then(res => res.json())
+            })
+                .then(res => res.json())
+                .catch(err => alert("" + err))
         },
         logout() {
-            return fetch(`${baseUrl}/session/logout`, { credentials: 'include' }).then(res => res.json())
+            return fetch(`${baseUrl}/session/logout`, { credentials: 'include' })
+                .then(res => res.json())
+                .catch(err => alert("" + err))
         },
     }
 }
